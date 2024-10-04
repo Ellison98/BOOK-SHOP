d43e46aeadfcb19afa9b27b85966358814c7036c
imseongsu <in05013300@gmail.com>
2024-10-04 20:54:28 +0900

    commit

--- routes/likes.js
+++ routes/likes.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.use(express.json());
-
 // 좋아요 추가
-router.post('/:id', (req, res) => {
+router.post('/likes/:id', (req, res) => {
     res.json('좋아요 추가');
 });
-
-// 좋아요 취소
-router.delete('/:id', (req, res) => {
-    res.json('좋아요 취소');
+// 좋아요 삭제
+router.post('/likes/:id', (req, res) => {
+    res.json('좋아요 삭제');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,21 @@
-const express = require('express');  // express 모듈을 가져온다.
+const express = require('express'); // express 모듈
 const router = express.Router();
-const conn = require('../mariadb');  // db 연결
+const conn = require('../mariadb'); // db 모듈
 const {
     join,
-    login, 
-    requestPasswordReset,
-    passwordReset 
-} = require('../controller/UserController');  // 회원가입 컨트롤러
+    login,
+    passwordResetRequest,
+    passwordReset,
+} = require('../controller/UserController');
 
 router.use(express.json());
 
 // 회원가입
 router.post('/join', join);
-
 // 로그인
 router.post('/login', login);
-
 // 비밀번호 초기화 요청
-router.post('/reset', requestPasswordReset);
-
+router.post('/reset', passwordResetRequest);
 // 비밀번호 초기화
 router.put('/reset', passwordReset);
 
